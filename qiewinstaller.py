359b1591db20188665b1144d9d08e5175111ee44
Marius TIVADAR <marius.tivadar@gmail.com>
2019-03-11 01:31:21 +0200

    work in progress

--- qiewinstaller.py
+++ qiewinstaller.py
@@ -5,10 +5,9 @@ import PyInstaller.__main__
 qiew = 'qiew'
 buildcmd = [
     '--name=%s' % qiew,
-#    '--onefile',
+    '--onefile',
     '--console',
 #    '--add-binary=%s' % os.path.join('plugins', '*.*', ),
-    '--add-data={};{}'.format(os.path.join('plugins','format','pe.py'), os.path.join('plugins','format')), 
 ]
 
 def add_datas(buildcmd, path, allowed=['.py', '.ui', '.yapsy-plugin', '.disabled']):
@@ -20,22 +19,34 @@ def add_datas(buildcmd, path, allowed=['.py', '.ui', '.yapsy-plugin', '.disabled
             buildcmd += [datas]
             print(datas)
 
-    
-
-
+         
+# plugins
 add_datas(buildcmd, os.path.join('plugins', 'format'))
 add_datas(buildcmd, os.path.join('plugins', 'unpack'))
 add_datas(buildcmd, os.path.join('.'), allowed=['.ui'])
 
+# capstone
 bin = '--add-data={};{}'.format(r'C:\Python36\Lib\site-packages\capstone\capstone.dll', '.')
-
 buildcmd += [bin]
 
+# modules
+bin = '--hidden-import={}'.format('pyelftools')
+buildcmd += [bin]
+
+# scripts
 L = os.listdir('.')
 for l in L:
     name, extension = os.path.splitext(l)
     if extension in ['.py']:
         buildcmd += [os.path.join(l)]
+
+# scripts
+L = os.listdir(os.path.join('.', 'plugins', 'format'))
+for l in L:
+    name, extension = os.path.splitext(l)
+    if extension in ['.py']:
+        buildcmd += [os.path.join('plugins', 'format', l)]
+
 
 buildcmd += [os.path.join('qiew.py')]
 
